8e27c736803af31d407380a27237722f0a49dcd6
Anthony Godin <gdn.anthony@gmail.com>
2021-11-15 17:32:29 +0100

    feature(team): add endpoint to post player in a team (#2)
    * feature(team): add endpoint to post player in a team
    
    Signed-off-by: Anthony Godin <gdn.anthony@gmail.com>
    
    * test: saves player in db
    
    Signed-off-by: Anthony Godin <gdn.anthony@gmail.com>
    
    * add schema
    
    Signed-off-by: Anthony Godin <gdn.anthony@gmail.com>

---
+++ __tests__/integration/api/teams/post_player.test.ts
@@ -0,0 +1,235 @@
+import { connection, server } from '../../../../@jest/setup';
+import { createTeam, getTeam } from '../../../helpers/database/team';
+import { PlayerToSaveDTO } from '../../../../src/dto/player';
+
+const PATH = '/api/team';
+
+type IsPlayerSavedInTeamRepository = (_: {
+  player: PlayerToSaveDTO;
+  teamYear: number;
+}) => void;
+const isPlayerSavedInTeamRepository: IsPlayerSavedInTeamRepository = async ({
+  player,
+  teamYear
+}) => {
+  const savedTeam = await getTeam({ year: teamYear });
+  const savedPlayers = savedTeam?.players;
+  if (savedTeam && savedPlayers) {
+    const savedPlayer = savedPlayers.find(
+      (playerIterator) => playerIterator.number === player.number
+    );
+
+    if (!savedPlayer) {
+      expect(savedPlayer).not.toBeNull();
+    } else {
+      const { id, ...rest } = savedPlayer;
+
+      expect(id).not.toBeNull();
+      expect(rest).toStrictEqual(player);
+    }
+  } else {
+    expect(savedTeam).not.toBeNull();
+    expect(savedPlayers).not.toBeNull();
+  }
+};
+
+describe(`POST a player on ${PATH}/:year`, () => {
+  test('returns 404 because there is not team in database for the year', async () => {
+    const player: PlayerToSaveDTO = {
+      number: 99,
+      name: 'Antonin',
+      lastName: 'Bouscarel',
+      position: 'forward',
+      isCaptain: false
+    };
+
+    const res = await server.inject({
+      method: 'POST',
+      url: `${PATH}/1`,
+      payload: player
+    });
+
+    expect(res.statusCode).toBe(404);
+  });
+
+  test('returns 400 because it misses number in the player dto', async () => {
+    const team = {
+      id: 'aa3b13a7-ecd9-440b-abf3-ceca80b66683',
+      coach: 'Marion Felix',
+      year: 2021,
+      players: []
+    };
+    await createTeam({ connection, team });
+
+    const player = {
+      name: 'Antonin',
+      lastName: 'Bouscarel',
+      position: 'forward',
+      isCaptain: false
+    };
+
+    const res = await server.inject({
+      method: 'POST',
+      url: `${PATH}/2021`,
+      payload: player
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  test('returns 400 because it misses name in the player dto', async () => {
+    const team = {
+      id: 'aa3b13a7-ecd9-440b-abf3-ceca80b66683',
+      coach: 'Marion Felix',
+      year: 2021,
+      players: []
+    };
+    await createTeam({ connection, team });
+
+    const player = {
+      number: 99,
+      lastName: 'Bouscarel',
+      position: 'forward',
+      isCaptain: false
+    };
+
+    const res = await server.inject({
+      method: 'POST',
+      url: `${PATH}/2021`,
+      payload: player
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  test('returns 400 because it misses lastName in the player dto', async () => {
+    const team = {
+      id: 'aa3b13a7-ecd9-440b-abf3-ceca80b66683',
+      coach: 'Marion Felix',
+      year: 2021,
+      players: []
+    };
+    await createTeam({ connection, team });
+
+    const player = {
+      number: 99,
+      name: 'Antonin',
+      position: 'forward',
+      isCaptain: false
+    };
+
+    const res = await server.inject({
+      method: 'POST',
+      url: `${PATH}/2021`,
+      payload: player
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  test('returns 400 because it misses position in the player dto', async () => {
+    const team = {
+      id: 'aa3b13a7-ecd9-440b-abf3-ceca80b66683',
+      coach: 'Marion Felix',
+      year: 2021,
+      players: []
+    };
+    await createTeam({ connection, team });
+
+    const player = {
+      number: 99,
+      name: 'Antonin',
+      lastName: 'Bouscarel',
+      isCaptain: false
+    };
+
+    const res = await server.inject({
+      method: 'POST',
+      url: `${PATH}/2021`,
+      payload: player
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  test('returns 400 because it misses isCaptain in the player dto', async () => {
+    const team = {
+      id: 'aa3b13a7-ecd9-440b-abf3-ceca80b66683',
+      coach: 'Marion Felix',
+      year: 2021,
+      players: []
+    };
+    await createTeam({ connection, team });
+
+    const player = {
+      number: 99,
+      name: 'Antonin',
+      lastName: 'Bouscarel',
+      position: 'forward'
+    };
+
+    const res = await server.inject({
+      method: 'POST',
+      url: `${PATH}/2021`,
+      payload: player
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  test('add a player in existing team and returns the player', async () => {
+    const team = {
+      id: 'aa3b13a7-ecd9-440b-abf3-ceca80b66683',
+      coach: 'Marion Felix',
+      year: 2021,
+      players: []
+    };
+    await createTeam({ connection, team });
+
+    const player: PlayerToSaveDTO = {
+      number: 99,
+      name: 'Antonin',
+      lastName: 'Bouscarel',
+      position: 'forward',
+      isCaptain: false
+    };
+
+    const res = await server.inject({
+      method: 'POST',
+      url: `${PATH}/2021`,
+      payload: player
+    });
+    const { id, ...receivedPlayer } = JSON.parse(res.payload);
+
+    expect(res.statusCode).toBe(200);
+    expect(id).not.toBeNull();
+    expect(receivedPlayer).toStrictEqual(player);
+  });
+
+  test('add a player in existing team and saves the player', async () => {
+    const year = 2021;
+    const team = {
+      id: 'aa3b13a7-ecd9-440b-abf3-ceca80b66683',
+      coach: 'Marion Felix',
+      year,
+      players: []
+    };
+    await createTeam({ connection, team });
+
+    const player: PlayerToSaveDTO = {
+      number: 99,
+      name: 'Antonin',
+      lastName: 'Bouscarel',
+      position: 'forward',
+      isCaptain: false
+    };
+
+    await server.inject({
+      method: 'POST',
+      url: `${PATH}/2021`,
+      payload: player
+    });
+
+    await isPlayerSavedInTeamRepository({ player, teamYear: year });
+  });
+});
--- src/controllers/teamsController.ts
+++ src/controllers/teamsController.ts
@@ -1,5 +1,23 @@
 import { FastifyServerOptions, FastifyInstance } from 'fastify';
 
+const bodyJsonSchema = {
+  type: 'object',
+  required: ['number', 'name', 'lastName', 'position', 'isCaptain'],
+  properties: {
+    number: { type: 'number' },
+    name: { type: 'string' },
+    lastName: { type: 'string' },
+    position: { type: 'string' },
+    isCaptain: { type: 'boolean' }
+  }
+};
+
+const opts = {
+  schema: {
+    body: bodyJsonSchema
+  }
+};
+
 const TeamsController = (
   server: FastifyInstance,
   options: FastifyServerOptions,
@@ -16,6 +34,31 @@ const TeamsController = (
       });
       if (team) {
         res.send({ team });
+      } else {
+        res.code(404).send();
+      }
+    }
+  );
+
+  server.post<{ Params: { year: string } }>(
+    '/api/team/:year',
+    opts,
+    async (req, res) => {
+      const year = req.params.year;
+      const team = await server.db.teams.findOne({
+        where: { year },
+        relations: ['players']
+      });
+
+      if (team) {
+        const playerToAdd = req.body;
+        const savedPlayer = await server.db.players.save(playerToAdd);
+        await server.db.teams.save({
+          ...team,
+          players: [...team.players, savedPlayer]
+        });
+
+        res.send(savedPlayer);
       } else {
         res.code(404).send();
       }
