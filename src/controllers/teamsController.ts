9153dca9c9acb539fe233674ccab118f444573af
Anthony Godin <gdn.anthony@gmail.com>
2021-11-15 14:30:53 +0100

    fix linter
    Signed-off-by: Anthony Godin <gdn.anthony@gmail.com>

--- src/controllers/teamsController.ts
+++ src/controllers/teamsController.ts
@@ -5,19 +5,22 @@ const TeamsController = (
   options: FastifyServerOptions,
   next: () => void
 ) => {
-  server.get('/api/team/:year', {}, async (req, res) => {
-    // @ts-ignore
-    const year = req.params.year;
-    const team = await server.db.teams.findOne({
-      where: { year },
-      relations: ['players']
-    });
-    if (team) {
-      res.send({ team });
-    } else {
-      res.code(404).send();
+  server.get<{ Params: { year: string } }>(
+    '/api/team/:year',
+    {},
+    async (req, res) => {
+      const year = req.params.year;
+      const team = await server.db.teams.findOne({
+        where: { year },
+        relations: ['players']
+      });
+      if (team) {
+        res.send({ team });
+      } else {
+        res.code(404).send();
+      }
     }
-  });
+  );
   next();
 };
 
